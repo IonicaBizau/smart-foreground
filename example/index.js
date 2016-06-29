0b1923729a0fe15dc99225a52a23ccbcd52cf5d0
Ionică Bizău <bizauionica@gmail.com>
2016-06-29 11:55:47 +0300

    Examples

--- example/index.js
+++ example/index.js
@@ -3,6 +3,28 @@
 const smartForeground = require("../lib");
 
 console.log(smartForeground("black"));
+// => #fffff
+
 console.log(smartForeground("white"));
+// => #00000
+
 console.log(smartForeground("yellow"));
+// => #00000
+
 console.log(smartForeground("brown"));
+// => #fffff
+
+console.log(smartForeground("#F82125"));
+// => #fffff
+
+console.log(smartForeground("#FFC966"));
+// => #00000
+
+console.log(smartForeground("darkred", ["yellow", "#FF0057", "darkred"]));
+// => yellow
+
+console.log(smartForeground("yellow", ["yellow", "#FF0057", "darkred"]));
+// => darkred
+
+console.log(smartForeground("rgb(255, 25, 255)", ["yellow", "#FF0057", "darkred"]));
+// => #FF0057
